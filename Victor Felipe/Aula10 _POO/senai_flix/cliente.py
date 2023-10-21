64d65c45db1d9a153c31fbdedaf081022e0780b5
AlencarFelipe <146783597+AlencarFelipe@users.noreply.github.com>
2023-10-20 21:57:15 -0300

    Add files via upload

---
+++ Victor Felipe/Aula10 _POO/senai_flix/cliente.py
@@ -0,0 +1,6 @@
+class Cliente:
+    def __init__(self, nome, endereco, email, login):
+        self.nome = nome
+        self.enderec = endereco
+        self.email = email
+        self.login = login
---
+++ Victor Felipe/Aula10 _POO/senai_flix/index.py
@@ -0,0 +1,3 @@
+"Criar um site para locação com aplicação de filmes , series e documentarios "
+#Personagem-Banco , Personagem-Cartao de Credito, Personagem-Locação, Personagem-Catalogo, Personagem-Login
+
