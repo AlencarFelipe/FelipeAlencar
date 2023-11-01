870d01ad8416fcaeaa191e28028c490da10916a5
AlencarFelipe <146783597+AlencarFelipe@users.noreply.github.com>
2023-11-01 19:22:05 -0300

    Delete Aula10 _POO directory

--- Aula10 _POO/senai_flix/cliente.py
+++
@@ -1,6 +0,0 @@
-class Cliente:
-    def __init__(self, nome, endereco, email, login):
-        self.nome = nome
-        self.endereco = endereco
-        self.email = email
-        self.login = login
--- Aula10 _POO/senai_flix/index.py
+++
@@ -1,3 +0,0 @@
-"Criar um site para locação com aplicação de filmes , series e documentarios "
-#Personagem-Banco , Personagem-Cartao de Credito, Personagem-Locação, Personagem-Catalogo, Personagem-Login
-
